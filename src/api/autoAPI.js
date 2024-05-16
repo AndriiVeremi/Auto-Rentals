99cfea7aa0becc53f168220577cf615b36b05a85
Andrii Veremii <114438382+AndriiVeremi@users.noreply.github.com>
2024-05-16 22:27:57 +0200

    addd

--- src/api/autoAPI.js
+++ src/api/autoAPI.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+console.log('BASE_URL::: ', BASE_URL);
+
+
+
 axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 
 export const getAuto = async (page = 1) => {
